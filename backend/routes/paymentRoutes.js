4eb00dbe0082b5ab40a6ce02603716b5cb542519
rennovatetechnologies <rennovatetechnologies@gmail.com>
2026-05-13 18:27:27 +0530

    remove backedn changes

--- backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-
-router.post('/create-order', paymentController.createOrder);
-router.post('/verify', paymentController.verifyPayment);
-
-module.exports = router;
